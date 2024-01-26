3aa60287dbbad3faa955b43388f300e1114588de
unknown <levannghia.tdc2018@gmail.com>
2024-01-26 11:49:37 +0700

    Update api

--- api/index.js
+++ api/index.js
@@ -17,122 +17,126 @@ const User = require("./models/user");
 const Chat = require("./models/message");
 
 mongoose.connect("mongodb+srv://meoem2712:2712000Nn@cluster0.cmentol.mongodb.net/").then(() => {
-    console.log("Connected to MongoDB");
+  console.log("Connected to MongoDB");
 }).catch((error) => {
-    console.log("Error connect MongoDB");
+  console.log("Error connect MongoDB");
 })
 
 
 app.listen(port, () => {
-    console.log("Server is runing port " + port);
+  console.log("Server is runing port " + port);
 })
 
 app.post('/register', async (req, res) => {
-    try {
-        const [name, email, password] = res.body;
-        const existingUser = await User.findOne({ email });
+  try {
+    // const [name, email, password] = req.body;
+    // const name = req.body.name;
+    // const email = req.body.email;
+    // const password = req.body.password;
+    const { name, email, password } = req.body;
+    const existingUser = await User.findOne({ email });
 
-        if (existingUser) {
-            res.status(400).json({ message: "User không tồn tại" })
-        }
-
-        const newUser = new User({
-            name,
-            email,
-            password
-        });
-
-        //generate the verification token
-        newUser.verificationToken = crypto.randomBytes(20).toString("hex");
-
-        //save the user to the database
-        await newUser.save();
-
-        //send the verification email to the registered user
-        sendVerificationEmail(newUser.email, newUser.verificationToken);
-
-        res.status(200).json({ message: "User registered successfully", userId: newUser._id });
-    } catch (error) {
-        console.log("Error registering user", error);
-        res.status(500).json({ message: "Registration failed" });
+    if (existingUser) {
+      res.status(400).json({ message: "User đã tồn tại" })
     }
+
+    const newUser = new User({
+      name,
+      email,
+      password
+  });
+
+    //generate the verification token
+    newUser.verificationToken = crypto.randomBytes(20).toString("hex");
+
+    //save the user to the database
+    await newUser.save();
+
+    //send the verification email to the registered user
+    sendVerificationEmail(newUser.email, newUser.verificationToken);
+
+    res.status(200).json({ message: "User registered successfully", userId: newUser._id });
+  } catch (error) {
+    console.log("Error registering user", error);
+    res.status(500).json({ message: "Registration failed" });
+  }
 })
 
 const sendVerificationEmail = async (email, verificationToken) => {
-    const transpoter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-            user: "sujananand0@gmail.com",
-            pass: "rnzcugnscqtqiefs",
-        },
-    });
+  const transpoter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "levannghia.tdc2018@gmail.com",
+      pass: "ordujivqhlfhafff",
+    },
+  });
 
-    const mailOptions = {
-        from: "matchmake.com",
-        to: email,
-        subject: "Email verification",
-        text: `Please click on the following link to verify your email : http://localhost:3000/verify/${verificationToken}`,
-    };
+  const mailOptions = {
+    from: "matchmake.com",
+    to: email,
+    subject: "Email verification",
+    text: `Please click on the following link to verify your email : http://localhost:3000/verify/${verificationToken}`,
+  };
 
-    //send the mail
-    try {
-        await transpoter.sendMail(mailOptions);
-    } catch (error) {
-        console.log("Error sending the verification email");
-    }
+  //send the mail
+  try {
+    await transpoter.sendMail(mailOptions);
+  } catch (error) {
+    console.log("Error sending the verification email");
+  }
 };
 
 //verify the user
 app.get("/verify/:token", async (req, res) => {
-    try {
-      const token = req.params.token;
-  
-      const user = await User.findOne({ verificationToken: token });
-      if (!user) {
-        return res.status(404).json({ message: "Invalid verification token" });
-      }
-  
-      //mark the user as verified
-      user.verified = true;
-      user.verificationToken = undefined;
-  
-      await user.save();
-  
-      res.status(200).json({ message: "Email verified Sucesfully" });
-    } catch (error) {
-      console.log("errror", error);
-      res.status(500).json({ message: "Email verification failed" });
+  try {
+    const token = req.params.token;
+
+    const user = await User.findOne({ verificationToken: token });
+    if (!user) {
+      return res.status(404).json({ message: "Invalid verification token" });
     }
-  });
-  
-  const generateSecretKey = () => {
-    const secretKey = crypto.randomBytes(32).toString("hex");
-  
-    return secretKey;
-  };
-  
-  const secretKey = generateSecretKey();
-  
-  //endpoint to login
-  app.post("/login", async (req, res) => {
-    try {
-      const { email, password } = req.body;
-  
-      //check if the user exists already
-      const user = await User.findOne({ email });
-      if (!user) {
-        return res.status(401).json({ message: "Invalid email or password" });
-      }
-  
-      //check in password is correct
-      if (user.password !== password) {
-        return res.status(401).json({ message: "Invalide password" });
-      }
-  
-      const token = jwt.sign({ userId: user._id }, secretKey);
-  
-      res.status(200).json({ token });
-    } catch (error) {
-      res.status(500).json({ message: "login failed" });
+
+    //mark the user as verified
+    user.verified = true;
+    user.verificationToken = undefined;
+
+    await user.save();
+
+    res.status(200).json({ message: "Email verified Sucesfully" });
+  } catch (error) {
+    console.log("errror", error);
+    res.status(500).json({ message: "Email verification failed" });
+  }
+});
+
+const generateSecretKey = () => {
+  const secretKey = crypto.randomBytes(32).toString("hex");
+
+  return secretKey;
+};
+
+const secretKey = generateSecretKey();
+
+//endpoint to login
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    //check if the user exists already
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid email or password" });
     }
-  });
+
+    //check in password is correct
+    if (user.password !== password) {
+      return res.status(401).json({ message: "Invalide password" });
+    }
+
+    const token = jwt.sign({ userId: user._id }, secretKey);
+
+    res.status(200).json({ token });
+  } catch (error) {
+    res.status(500).json({ message: "login failed" });
+  }
+});
